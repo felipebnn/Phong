ec7dd3f7895d2155779c6fdbcdbf3484b4ce977d
Felipe Nardi Batista <felipebbnn@gmail.com>
2018-12-07 13:48:03 -0200

    Reduce complexity of sorting inside `buildKdNode` method.

--- src/kdnode.cpp
+++ src/kdnode.cpp
@@ -8,33 +8,21 @@ std::unique_ptr<KdNode> KdNode::buildKdNode(Triangle* trianglesX, size_t triangl
 	if (triangleCount == 1) {
 		node->triangle = *trianglesX;
 		node->bbox = BoundingBox::fromTriangle(*trianglesX);
+	} else if (triangleCount > 1) {
+		Triangle* mid = trianglesX + triangleCount / 2;
+
+		std::nth_element(trianglesX, mid, trianglesX + triangleCount, [depth] (const Triangle& t0, const Triangle& t1) {
+			return t0.v0->pos[depth] + t0.v1->pos[depth] + t0.v2->pos[depth] < t1.v0->pos[depth] + t1.v1->pos[depth] + t1.v2->pos[depth];
+		});
+
+		depth = (depth + 1) % 3;
+
+		node->left = buildKdNode(trianglesX, static_cast<size_t>(mid - trianglesX), depth);
+		node->right = buildKdNode(mid, static_cast<size_t>(trianglesX + triangleCount - mid), depth);
+
+		node->bbox = node->left->bbox;
+		node->bbox.expand(node->right->bbox);
 	}
-
-	if (triangleCount < 2) {
-		return node;
-	}
-
-	std::sort(trianglesX, trianglesX + triangleCount, [depth] (const Triangle& t0, const Triangle& t1) {
-		glm::vec3 c0 = t0.v0->pos + t0.v1->pos + t0.v2->pos;
-		glm::vec3 c1 = t1.v0->pos + t1.v1->pos + t1.v2->pos;
-
-		switch (depth % 3) {
-			case 0:
-				return c0.x < c1.x;
-			case 1:
-				return c0.y < c1.y;
-			default: //case 2:
-				return c0.z < c1.z;
-		}
-	});
-
-	Triangle* mid = trianglesX + triangleCount / 2;
-
-	node->left = buildKdNode(trianglesX, mid - trianglesX, depth + 1);
-	node->right = buildKdNode(mid, trianglesX + triangleCount - mid, depth + 1);
-
-	node->bbox = node->left->bbox;
-	node->bbox.expand(node->right->bbox);
 
 	return node;
 }
